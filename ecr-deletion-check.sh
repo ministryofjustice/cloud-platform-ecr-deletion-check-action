57e15f2f929a5cf81b831cd0a97cf105adbd6974
Aafreen Ansari <aafreen.ansari@digital.justice.gov.uk>
2024-08-05 11:45:43 +0100

    added Dockerfile, metadata and action code

---
+++ ecr-deletion-check.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -l
+
+echo "Hello"
+time=$(date)
+
+DELETED_FILES=$(git diff --name-status ${{ github.event.before }} ${{ github.sha }} | awk '$1 == "D" { print $2 }')
+          echo "Deleted files:"
+          echo "${DELETED_FILES}"
+          ECR_DELETED=$(cat "${DELETED_FILES}" | grep -q 'github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials' && echo true || echo false)
+          echo "::set-output name=ecr_deleted::${ECR_DELETED}"
+
+
+# add comment in the PR
+
