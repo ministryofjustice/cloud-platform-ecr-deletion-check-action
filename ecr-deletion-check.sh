c94e64e9818837f3c0f7c9250e24dc5e699fc81f
Aafreen Ansari <aafreen.ansari@digital.justice.gov.uk>
2024-08-05 13:24:43 +0100

    Update ecr-deletion-check.sh

--- ecr-deletion-check.sh
+++ ecr-deletion-check.sh
@@ -1,14 +1,13 @@
 #!/bin/sh -l
 
+set -e
+set +x
 echo "Hello"
 
-time=$(date)
-
 DELETED_FILES=$(git diff --name-status origin/main ${{ github.sha }} | awk '$1 == "D" { print $2 }')
-          echo "Deleted files:"
-          echo "${DELETED_FILES}"
-          ECR_DELETED=$(cat "${DELETED_FILES}" | grep -q 'github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials' && echo true || echo false)
-          echo "::set-output name=ecr_deleted::${ECR_DELETED}"
+echo "Deleted files: $DELETED_FILES" 
+ECR_DELETED=$(cat "$DELETED_FILES" | grep -q 'github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials' && echo true || echo false)
+echo "::set-output name=ecr_deleted::${ECR_DELETED}"
 
 
 # add comment in the PR
